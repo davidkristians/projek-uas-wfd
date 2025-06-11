e98c8e47e9917867c5dd87e9028ff6996e90e15f
David-Winata <geletrikx05@gmail.com>
2025-06-11 16:43:38 +0700

    logout

--- resources/views/karyawan/dashboard.blade.php
+++ resources/views/karyawan/dashboard.blade.php
@@ -3,11 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>VroomWash</title>
+    <link rel="icon" href="{{ asset('icon_vroom.png') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
     <h1>Selamat datang Karyawan!</h1>
-    <a href="{{ route('logout') }}">Logout</a>
+    <form method="POST" action="{{ route('logout') }}" id="logout-form">
+        @csrf
+        <button type="button" class="btn btn-danger" onclick="confirmLogout()">Logout</button>
+    </form>
+
+
+    <script>
+        function confirmLogout() {
+            if (confirm("Are You Sure Want to Logout?")) {
+                document.getElementById('logout-form').submit();
+            }
+        }
+    </script>
 
 </body>
 </html>
